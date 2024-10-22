5f59d4155f89a6b07830e2783d3207cdbf948ce3
Wellyngton Marcondes <admin@simplenetwork.com.br>
2024-10-22 12:22:41 -0300

    Layout e funções do alerta

--- src/js/alerta.js
+++ src/js/alerta.js
@@ -1,10 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let btadd = document.getElementById('btadd');
-    let lista = document.getElementById('lista');
-
-    btadd.addEventListener('click', function() {
-        lista.appendChild(document.createElement('li')).appendChild(document.createElement('p')).textContent = 'Alerta criado ' + (lista.childElementCount);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    var btadd = document.getElementById("btadd");
     
+    btadd.addEventListener("click", function () {
+        var titulo = document.getElementById("titulo").value;
+        var email = document.getElementById("email").value;
+        var dinicio = document.getElementById("data-inicio").value;
+        var dfim = document.getElementById("data-fim").value;
+        var lista = document.getElementById("lista");
+        var descricao = document.getElementById("descricao").value;
+    
+        lista
+        .appendChild(document.createElement("li"))
+        .appendChild(document.createElement("p")).textContent =
+        "Alerta criado " + lista.childElementCount;
+
+        let documento = {
+          "id": lista.childElementCount,
+          "titulo": titulo,
+          "email": email,
+          "dinicio": dinicio,
+          "dfim": dfim,
+          "descricao": descricao,
+        };
+        
+        console.log(documento);
+  });
 
 });
